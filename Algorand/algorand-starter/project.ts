7c0a7dba9997e786e44514a9268202efea5eaf1d
James Bayly <bayly.james@gmail.com>
2023-10-13 16:20:20 +0800

    Update Message about endpoints

--- Algorand/algorand-starter/project.ts
+++ Algorand/algorand-starter/project.ts
@@ -29,12 +29,12 @@ const project: AlgorandProject = {
     // For the testnet use the following
     // chainId: "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI="
     chainId: "wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=",
-    // This endpoint must be a public non-pruned archive node
-    // We recommend providing more than one endpoint for improved reliability, performance, and uptime
-    // Public nodes may be rate limited, which can affect indexing speed
-    // When developing your project we suggest getting a private API key
-    // You can get them from OnFinality for free https://app.onfinality.io
-    // https://documentation.onfinality.io/support/the-enhanced-api-service
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     */
     endpoint: ["https://mainnet-idx.algonode.cloud"],
     // Optionally provide the HTTP endpoint of a full chain dictionary to speed up processing
     dictionary: "https://api.subquery.network/sq/subquery/Algorand-Dictionary",
